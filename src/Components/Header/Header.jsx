7e6c75b656efed1d5f8c53464149a204c3a14867
seifallahmedini <seifallahmedini@gmail.com>
2020-03-17 18:55:00 +0100

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="masthead">
+      <div className="overlay"></div>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 col-md-10 mx-auto">
+            <div className="site-heading">
+              <h1>Clean Blog</h1>
+              <span className="subheading">
+                A Blog Theme by Start Bootstrap
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
